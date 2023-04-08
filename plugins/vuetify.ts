6ec2f6d4ef662c5429217e78c36ec4a456067147
sakura <admin@sakura.rip>
2023-04-08 23:26:32 +0900

    update: vuetify configs

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,14 +1,10 @@
 import {createVuetify} from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import {aliases, mdi} from "vuetify/iconsets/mdi-svg";
 import {md3} from 'vuetify/blueprints'
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     ssr: false,
-    components,
-    directives,
     blueprint: md3,
     icons: {
       defaultSet: "mdi",
